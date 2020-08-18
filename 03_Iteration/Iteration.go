dbfa1eaf55cf1d3afb4f58bf215fd2b5e0a0b395
Delfin Roc <683690@unizar.es>
2020-08-18 15:31:49 +0200

    Added Iteration

---
+++ 03_Iteration/Iteration.go
@@ -0,0 +1,12 @@
+package iteration
+
+const repeatCount = 5
+
+//Repeat funcion
+func Repeat(character string) string {
+	var repeated string
+	for i := 0; i < repeatCount; i++ {
+		repeated += character
+	}
+	return repeated
+}
